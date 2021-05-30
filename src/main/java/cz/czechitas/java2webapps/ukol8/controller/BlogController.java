6fc16b26627956608a620578e4e551c9fab0802b
sonitko <sonitko.cz@gmail.com>
2021-05-30 09:24:17 +0200

    Úkol č.8 - částečně funkční page-ing

--- src/main/java/cz/czechitas/java2webapps/ukol8/controller/BlogController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol8/controller/BlogController.java
@@ -4,7 +4,12 @@ import cz.czechitas.java2webapps.ukol8.service.PostService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.UrlPathHelper;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class BlogController {
@@ -25,6 +30,22 @@ public class BlogController {
     public ModelAndView showPost(String slug) {
         return new ModelAndView("onePost")
                 .addObject("onePost", postService.findBySlug(slug, Pageable.unpaged()));
+    }
+
+    /**
+     * Získání aktuální URL s query parametry bez parametrů {@code size} a {@code page}.
+     */
+    @ModelAttribute("currentURL")
+    public String currentURL(HttpServletRequest request) {
+        UrlPathHelper helper = new UrlPathHelper();
+        return UriComponentsBuilder
+                .newInstance()
+                .path(helper.getOriginatingRequestUri(request))
+                .query(helper.getOriginatingQueryString(request))
+                .replaceQueryParam("size")
+                .replaceQueryParam("page")
+                .build()
+                .toString();
     }
 
 }
